0c3c68f8d13a4cf75fcfa443c4b20f45a89bdc3a
ahmedmanubariya <ahmed-m2011@hotmail.co.uk>
2026-09-08 16:11:22 +0100

    feat(eqms): add database layer

---
+++ eqms/api/app/db.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DATABASE_URL = os.getenv("EQMS_DATABASE_URL", "sqlite:///./eqms.db")
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, future=True, pool_pre_ping=True, connect_args=connect_args)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
